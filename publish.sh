267c33f6a2ab7fc4795defba76ef8bb5a5c768f9
aquint-zama <alexandre.quint@zama.ai>
2023-01-30 15:49:47 +0100

    fix: suck at sed

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 
 VERSION_FILE="version.py"
 NEWTAG=$1
-sed -i "" "s/__version__=\"v[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\"/__version__=\"${NEWTAG//./\\.}\"/" $VERSION_FILE
+sed -i "s/__version__=\"v[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\"/__version__=\"${NEWTAG//./\\.}\"/" $VERSION_FILE
